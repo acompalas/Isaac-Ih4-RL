326999d149983c2f10856dea9a5d8981731943a1
acompalas <128974388+acompalas@users.noreply.github.com>
2026-08-01 19:59:10 -0700

    editing init

--- rainbow_demorl/envs/__init__.py
+++ rainbow_demorl/envs/__init__.py
@@ -1,7 +1,3 @@
-from .maniskill import find_max_episode_steps_value, make_envs as make_maniskill_envs
-from .catheter import make_envs as make_catheter_envs
-
-
 def make_envs(args, run_name: str):
     if args.env_id.startswith("Catheter"):
         from .catheter import make_envs as make_catheter_envs
