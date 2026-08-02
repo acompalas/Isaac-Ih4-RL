4d34468258447d4a9a26b8446567a88feefe3971
acompalas <128974388+acompalas@users.noreply.github.com>
2026-08-01 18:19:47 -0700

    make maniskill import lazy

--- rainbow_demorl/envs/__init__.py
+++ rainbow_demorl/envs/__init__.py
@@ -4,5 +4,7 @@ from .catheter import make_envs as make_catheter_envs
 
 def make_envs(args, run_name: str):
     if args.env_id.startswith("Catheter"):
+        from .catheter import make_envs as make_catheter_envs
         return make_catheter_envs(args, run_name)
+    from .maniskill import make_envs as make_maniskill_envs
     return make_maniskill_envs(args, run_name)
